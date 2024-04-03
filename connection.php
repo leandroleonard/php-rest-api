1050e86ad624df6a5e73329fc2ec7a6eb3828abf
Leonardo Leandro <leandroventur19@gmail.com>
2024-04-03 17:26:41 +0100

    Setup connection

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$dns    = "localhost";
+$user   = "root";
+$pass   = "";
+
+try {
+    $conn = new PDO("mysql:host=$dns;dbname=api", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
