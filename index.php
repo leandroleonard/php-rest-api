603e3af0a2441901e9823d4000af51a279ca2ab9
Leonardo Leandro <leandroventur19@gmail.com>
2024-04-03 17:45:33 +0100

    Endpoint: Get user
    Return all users in db

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-	require_once "connection.php";
+require_once "connection.php";
 
-	
+// GET users
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && empty($_GET)) {
+	try {
+		$stmt = $conn->query('SELECT * FROM users');
+		$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		header('Content-Type: application/json');
+		echo json_encode($users);
+	} catch (PDOException $e) {
+		echo json_encode(['error' => $e->getMessage()]);
+	}
+}
