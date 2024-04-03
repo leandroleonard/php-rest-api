61cd49bc80a432c52e79023d0ba1d77e636dfbe7
Leonardo Leandro <leandroventur19@gmail.com>
2024-04-03 17:50:20 +0100

    Endpoint create user

--- index.php
+++ index.php
@@ -13,3 +13,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && empty($_GET)) {
 		echo json_encode(['error' => $e->getMessage()]);
 	}
 }
+
+// HTTP METHOD: POST
+// Body Param: name, email
+// Add new user
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$data = json_decode(file_get_contents('php://input'), true);
+
+	if (empty($data['name']) || empty($data['email'])) {
+		echo json_encode(['error' => 'Username and email are required']);
+		exit;
+	}
+
+	$name = $data['name'];
+	$email = $data['email'];
+
+	try {
+		$stmt = $conn->prepare('INSERT INTO users (name, email) VALUES (:name, :email)');
+		$stmt->bindParam(':name', $name);
+		$stmt->bindParam(':email', $email);
+		$stmt->execute();
+
+		$userID = $conn->lastInsertId();
+		echo json_encode(['id' => $userID, 'name' => $name, 'email' => $email]);
+	} catch (PDOException $e) {
+		echo json_encode(['error' => $e->getMessage()]);
+	}
+}
